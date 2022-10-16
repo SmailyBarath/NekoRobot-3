eae154672b7f8d69f9580fbfae07e1be4110d56a
SmailyBarath <115382856+SmailyBarath@users.noreply.github.com>
2022-10-16 10:57:04 +0530

    Update config.py

--- NekoRobot/config.py
+++ NekoRobot/config.py
@@ -18,13 +18,12 @@ class Config(object):
     API_HASH = "123458494"
     TOKEN = "5096800645:AAEqiDu0tIAz6Tj5mNPP38R9AyfzEWr1SO0"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1732814103  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "DarlingPrince"
-    SUPPORT_CHAT = "ProgrammerSupport"  # Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001739802989
+    OWNER_USERNAME = "Call_Me_ProCoder"
+    SUPPORT_CHAT = "JackSparrowSupport"  # Your own group for support, do not add the @
+    JOIN_LOGGER = ( -1001572192111
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001739802989
+        -1001572192111
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -98,13 +97,13 @@ class Config(object):
     API_HASH = "12345abcdefg23459"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1732814103  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "DarlingPrince"
-    SUPPORT_CHAT = "ProgrammerSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "Call_Me_ProCoder"
+    SUPPORT_CHAT = "JackSparrowSupport"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001739802989
+        -1001572192111
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001739802989
+        -1001572192111
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
