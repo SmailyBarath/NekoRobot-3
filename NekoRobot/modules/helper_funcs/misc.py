81ba824957ea94c2e5c0bc4bce38cbc89bf91a14
SmailyBarath <115382856+SmailyBarath@users.noreply.github.com>
2022-10-16 12:12:22 +0530

    Update misc.py

--- NekoRobot/modules/helper_funcs/misc.py
+++ NekoRobot/modules/helper_funcs/misc.py
@@ -97,7 +97,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
     else:
-        pairs += [[EqInlineKeyboardButton("[► Back ◄]", callback_data="neko_back")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="neko_back")]]
 
     return pairs
 
